15dba7fdb965f500b6e167d7ac194ce55c786612
David Darras <david.darras@insee.fr>
2023-01-16 11:38:19 +0100

    fix(test): [#5] fix plop

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,8 +2,8 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   parserPreset: {
     parserOpts: {
-      headerPattern: /^(\w*)(?:\((.*)\))?: (\[#[0-9]{1,4}\].*)$/,
-      headerCorrespondence: ["type", "scope", "subject"],
+      headerPattern: /^(\w*)(?:\((.*)\))?: \[#([0-9]{1,4})\](.*)$/,
+      headerCorrespondence: ["type", "scope", "reference", "subject"],
       issuePrefixes: ["#"],
     },
   },
